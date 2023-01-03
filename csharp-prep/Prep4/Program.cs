a0912003174b4bf97cd21c8c4761ab27f6fbdc2c
Thomas Montoya <thomasamh2899@gmail.com>
2023-01-03 21:44:05 +0100

    Assignment Completed: Prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,23 @@
 using System;
+using System.Linq;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
-    }
+        //Define variables
+        List<int> userIntList = new List<int>();
+        string userInput = "";
+        do {
+
+            Console.WriteLine("Enter a number (enter 0 to quit)");
+            userInput = Console.ReadLine();
+            userIntList.Add(int.Parse(userInput));
+
+        } while (int.Parse(userInput) != 0 );
+
+        Console.WriteLine($"The sum of the list is: {userIntList.Sum()}");
+        Console.WriteLine($"The average of the list is: {userIntList.Average()}");
+        Console.WriteLine($"The largest number is: {userIntList.Max()}");
+    } 
 }
