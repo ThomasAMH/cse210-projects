3070b0921ef144bef6fecc4786f1eadb24e21a9a
Thomas Montoya <thomasamh2899@gmail.com>
2023-01-04 21:02:15 +0100

    Assignment Completed: Prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        string userName = "";
+        int userNumber = 0;
+        static void DisplayWelcome() {
+            Console.WriteLine("Welcome to the Program!");
+        };
+
+        static string PromptUserName() {
+            Console.WriteLine("What is your name?");
+            return Console.ReadLine();
+        };
+
+        static int PromptUserNumber() {
+            Console.WriteLine("What is your favorite number?");
+            return int.Parse(Console.ReadLine());
+        };
+
+        static int SquareNumber(int someNumber) {
+            return someNumber * someNumber;
+        };
+
+        static void DisplayResult(int squaredNumber, string userName) {
+            Console.WriteLine($"Your name is: {userName}");
+            Console.WriteLine($"Your number squared is: {squaredNumber}");
+        };
+        
+        //Main
+        DisplayWelcome();
+        userName = PromptUserName();
+        userNumber = PromptUserNumber();
+        DisplayResult(SquareNumber(userNumber), userName);
+
     }
 }
